0c5aa92defcc4885c82c8917b77343370126bea8
Nikita O'Keeffe <snow@Nikitas-MacBook-Air.local>
2023-08-16 19:23:14 +1000

    added GPT prompt API call function

--- cogs/ask.py
+++ cogs/ask.py
@@ -1,7 +1,9 @@
+import openai
 from disnake.ext import commands
 from disnake import (
     ApplicationCommandInteraction
 )
+
 
 def setup(bot: "Bot"):
     bot.add_cog(Ask(bot))
@@ -11,22 +13,33 @@ class Ask(commands.Cog):
     def __init__(self, bot: "Bot"):
         self.bot = bot
 
-    """
-    Driver function for querying OpenAI 
-    """
+    def ask_openai(self, prompt):
+        messages = [
+            {"role": "system", "content": "you are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            message=messages,
+        )
+
+        return response['choices'][0]['message']['content'].strip()
+
     @commands.slash_command(name="ask", description="Ask the AI anything!")
     async def ask(self, inter: ApplicationCommandInteraction, prompt):
-
         """
         Send a response when a user types in /ask <args>. 
         """
-        await inter.send("Give me a second to think...") 
+        await inter.response.defer()
 
-        
-        response = await prompt()
+        await inter.followup.send("Give me a second to think...")
+
+        response = self.ask_openai(prompt)
+
+        await inter.followup.send(content=response)
 
         """
         @TODO: Replace above message from the bot with the result. Add some fancy loading things?
         inter.send(response)
         """
-
