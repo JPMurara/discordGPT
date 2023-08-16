c97da08354203d20b9750dd43b6df57f2c92576c
Joel McElroy <joelanthony.mac@gmail.com>
2023-08-16 18:47:17 +1000

    Actually let's prompt openAI first...

--- cogs/ask.py
+++ cogs/ask.py
@@ -11,7 +11,22 @@ class Ask(commands.Cog):
     def __init__(self, bot: "Bot"):
         self.bot = bot
 
+    """
+    Driver function for querying OpenAI 
+    """
     @commands.slash_command(name="ask", description="Ask the AI anything!")
-    async def ask(self, inter: ApplicationCommandInteraction):
-        """" Here is where we can parse the GPT results to the user """
-        await inter.send(gpt_stuff) 
+    async def ask(self, inter: ApplicationCommandInteraction, prompt):
+
+        """
+        Send a response when a user types in /ask <args>. 
+        """
+        await inter.send("Give me a second to think...") 
+
+        
+        response = await prompt()
+
+        """
+        @TODO: Replace above message from the bot with the result. Add some fancy loading things?
+        inter.send(response)
+        """
+
